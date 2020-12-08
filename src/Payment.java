5d09605e36d39ff2457f5e126d2752f0cbce699b
DonkeyBeDonky <61010676+DonkeyBeDonky@users.noreply.github.com>
2020-12-08 15:58:44 +0000

    Update Payment.java

--- src/Payment.java
+++ src/Payment.java
@@ -1,5 +1,43 @@
+
 public class Payment {
-    // The system should maintain a record of all payments
-    // of the property charge on a yearly basis.
-    // All records should be held in csv files.
+    private double OutStandingFees;
+    private double Fees;
+    private double totalPaid;
+    private double LivingAreaTax;
+    private double PropertyTax;
+    private String Location;
+    public Payment(double OutStandingFees,double PropertyTax, double Fees, double totalPaid,double LivingAreaTax){
+        this.OutStandingFees = OutStandingFees;
+        this.Fees = Fees;
+        this.totalPaid = totalPaid;
+        this.LivingAreaTax = LivingAreaTax;
+        this.PropertyTax = PropertyTax;
+    }
+      public void LivingAreaTax(String Location){  
+         
+         if (Location == "City"){
+            Fees = Fees + 100;
+        } else if(Location == "Large Town"){
+            Fees = Fees + 80;
+        }else if(Location == "Small Town"){
+            Fees = Fees + 60;
+        }else if(Location == "Village"){
+            Fees = Fees + 50;
+        }else if(Location == "CountrySide"){
+            Fees = Fees + 25;
+        }
+        
+     }
+     public void PropertyTax(){
+         
+        }
+        
+     
+    public void Fees(){
+        
+    }
+    public void totalPaid(){
+        
+    }
+    
 }
